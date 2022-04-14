4cd1a66a5514b1b494bf16b5eb87a8e0349890bf
jeanmfdias <jeanmfdias@gmail.com>
2022-04-14 13:44:44 -0300

    added movie controller

---
+++ movies_api/Controllers/MovieController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using movies_api.Models;
+using System;
+using System.Collections.Generic;
+
+namespace movies_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class MovieController : ControllerBase
+    {
+        private static List<Movie> movies = new List<Movie>();
+
+        [HttpPost]
+        public void addMovie([FromBody] Movie movie)
+        {
+            movies.Add(movie);
+            Console.WriteLine(movie.Title);
+        }
+    }
+}
---
+++ movies_api/Models/Movie.cs
@@ -0,0 +1,10 @@
+﻿namespace movies_api.Models
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public string Category { get; set; }
+        public int Time { get; set; }
+    }
+}
